620e7c050a2b40508da373fb5a31c30f5290903b
Mohsen Saniee <Saniee2@outlook.com>
2021-08-14 11:07:45 +0430

    Add Category Entity

---
+++ CleanArchitecture/CleanArchitecture.Domain/Entities/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set;}
+    }
+}
