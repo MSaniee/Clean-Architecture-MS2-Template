69588d514822404278078afc158fca1dffa83562
Mohsen Saniee <Saniee2@outlook.com>
2021-08-15 11:46:40 +0430

    Add Category Controller

---
+++ CleanArchitecture/CleanArchitecture.Mvc/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Mvc.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _categoryService.GetCategories();
+
+            return View(model);
+        }
+    }
+}
--- CleanArchitecture/CleanArchitecture.Mvc/Startup.cs
+++ CleanArchitecture/CleanArchitecture.Mvc/Startup.cs
@@ -28,7 +28,7 @@ namespace CleanArchitecture.Mvc
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {           
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("BookStoreIdentityDBConnection")));
