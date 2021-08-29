d486e08653a594ef314b1b26d4d2a5842477bd79
Mohsen Saniee <Saniee2@outlook.com>
2021-08-29 15:31:36 +0430

    Add Some Changes

---
+++ MS2Project/src/MS2Project.Common/TestItems/TestItem.cs
@@ -0,0 +1,11 @@
+﻿namespace MS2Project.Common.TestItems
+{
+    public class TestItem
+    {
+        /*
+         * This class is for develoers, They can add their testItem for example : 
+         * fake phone number
+         * and emial
+         */
+    }
+}
